687326abe3c81aef95cb5f2643bb3da43b652d31
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-10-15 19:51:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // a mile is equal 1.609344 kilometers
+        double kilometers = (100 * 1.609344); // This is the expression: "kilometers = (100 * 1.609344)"
+        // adding the data type, "double" makes it a valid java "statement" along with the ";".
+        int highScore = 50;
+
+        if (highScore == 50) { // This is the expression: "highScore == 50"
+            System.out.println("This is an expression");
+        }
+    }
+}
