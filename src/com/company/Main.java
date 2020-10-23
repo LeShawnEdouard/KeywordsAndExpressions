9dff9de56a3e26d907e7636de5a85e1ed4a491f8
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-10-23 19:37:01 -0400

    Commit: "Changing the values of variables by setting the variable to the current value of the variable plus one"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,19 +12,36 @@ public class Main {
             System.out.println("This is an expression");
         }
 
+
+
         // Will include Code Blocks & If then Else Statements in this project //
         boolean gameOver = true;
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score < 5000 && score > 1000) { // You can remove the code blocks "{}" if you have one statement in the following line
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000) {
-            System.out.println("Your score was less than 1000");
-        } else {
-            System.out.println("Got here");
+//        if (score < 5000 && score > 1000) { // You can remove the code blocks "{}" if you have one statement in the following line
+//            System.out.println("Your score was less than 5000 but greater than 1000");
+//        } else if (score < 1000) {
+//            System.out.println("Your score was less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
         }
+
+        score = score + 5000;
+        levelCompleted = levelCompleted + 3;
+        bonus = bonus + 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+
+        }
+
 
     }
 }
