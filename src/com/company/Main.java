8d70b08b742d6c63c4c5d2898b48eafe96a456a9
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-10-24 20:44:19 -0400

    Commit: "Converted code to use a method and pass variables as arguments to make code more efficient"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,35 +15,19 @@ public class Main {
 
 
         // Will include Code Blocks & If then Else Statements in this project //
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        calculateScore(); // Called the method to run the if statement
+        calculateScore(true, 800, 5, 100); // Called the method to run the if statement
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-
-        }
+        calculateScore(true, 10000, 8, 200);
 
 
     }
 
-    public static void calculateScore() {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
         }
 
