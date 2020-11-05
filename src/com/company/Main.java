c3ec44dd8ebc7d7df57d4b7bf248d472b72a5d61
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-04 20:03:56 -0500

    Commit: "Created two method and passed our parameters through and stored within the highScorePosition variable. Each person was assigned a number if meeting a certain condition"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,75 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // a mile is equal 1.609344 kilometers
-        double kilometers = (100 * 1.609344); // This is the expression: "kilometers = (100 * 1.609344)"
-        // adding the data type, "double" makes it a valid java "statement" along with the ";".
-        int highScore = 50;
 
-        if (highScore == 50) { // This is the expression: "highScore == 50"
-            System.out.println("This is an expression");
-        }
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
+        // Challenge //
+        // Create a method called displayHighScorePosition
+        // it should a players name as a parameter, and a 2nd parameter as a position in the high score table
+        // You should display the players name along wit a message like " managed to get into position " and the
+        // position they got and a further message " on the high score table".
+        //
+        // Create a 2nd method called calculateHighScorePosition
+        // it should be sent one argument only, the player score
+        // it should return an int
+        // the return data should be
+        // 1 if the score is > 1000
+        // 2 if the score is > 500 and < 1000
+        // 3 if the score is > 100 and < 500
+        // 4 in all other cases
+        // call both methods and display the results of the following
+        // a score of 1500, 900, 400, and 50
+        //
 
-        // Will include Code Blocks & If then Else Statements in this project //
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
 
-        calculateScore(true, 800, 5, 100); // Called the method to run the if statement
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
 
-        calculateScore(true, 10000, 8, 200);
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
 
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
 
     }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(playerScore > 1000) {
+            return 1;
+        } else if(playerScore > 500 && playerScore < 1000) {
+            return 2;
+        } else if(playerScore > 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+
+
+
+
+
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1; // We changed "void" to "int" in this method to return data
