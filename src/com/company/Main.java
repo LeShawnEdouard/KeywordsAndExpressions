308f3e503d885536bcc668b31f5fd6096bb5194f
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-06 07:03:11 -0500

    Commit: "Fixed logic hole by adding a '=' sign in the event a score was exactly 1000"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,9 +22,9 @@ public class Main {
         // it should be sent one argument only, the player score
         // it should return an int
         // the return data should be
-        // 1 if the score is > 1000
-        // 2 if the score is > 500 and < 1000
-        // 3 if the score is > 100 and < 500
+        // 1 if the score is >= 1000
+        // 2 if the score is >= 500 and < 1000
+        // 3 if the score is >= 100 and < 500
         // 4 in all other cases
         // call both methods and display the results of the following
         // a score of 1500, 900, 400, and 50
@@ -42,6 +42,8 @@ public class Main {
         highScorePosition = calculateHighScorePosition(50);
         displayHighScorePosition("Gilbert", highScorePosition);
 
+        highScorePosition =calculateHighScorePosition(1000);
+        displayHighScorePosition("Louise", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
@@ -50,11 +52,11 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-        if(playerScore > 1000) {
+        if(playerScore >= 1000) {
             return 1;
-        } else if(playerScore > 500 && playerScore < 1000) {
+        } else if(playerScore >= 500 && playerScore < 1000) {
             return 2;
-        } else if(playerScore > 100 && playerScore < 500) {
+        } else if(playerScore >= 100 && playerScore < 500) {
             return 3;
         } else {
             return 4;
