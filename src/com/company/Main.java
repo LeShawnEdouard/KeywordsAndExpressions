20bffc12bef9a17dcf9e05361b6654342f54324b
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-07 16:24:19 -0500

    Commit: "Removed the two expressions in the first else if statements due to redundancy"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -44,6 +44,12 @@ public class Main {
 
         highScorePosition =calculateHighScorePosition(1000);
         displayHighScorePosition("Louise", highScorePosition);
+
+        highScorePosition =calculateHighScorePosition(500);
+        displayHighScorePosition("Carol", highScorePosition);
+
+        highScorePosition =calculateHighScorePosition(100);
+        displayHighScorePosition("Frank", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
@@ -54,9 +60,9 @@ public class Main {
     public static int calculateHighScorePosition(int playerScore) {
         if(playerScore >= 1000) {
             return 1;
-        } else if(playerScore >= 500 && playerScore < 1000) {
+        } else if(playerScore >= 500) {
             return 2;
-        } else if(playerScore >= 100 && playerScore < 500) {
+        } else if(playerScore >= 100) {
             return 3;
         } else {
             return 4;
